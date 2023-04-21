4a3d509169cb3db35f179992dd5ed3cec5eb10ce
diusdi <riadidion@gmail.com>
2023-04-21 13:12:31 +0700

    ADD: dokumentasi dengan swagger

--- main.go
+++ main.go
@@ -5,8 +5,23 @@ import (
 	"api-mahasiswa/routers"
 	"log"
 
+	_ "api-mahasiswa/docs"
+
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
+
+// @title API Mahasiswa
+// @version 1.0
+// @description API untuk mengatur data mahasiswa Jobhun. Untuk source code dapat dilihat di https://github.com/diusdi/api-mahasiswa
+
+// @license.name Licensi MIT
+// @license.url https://opensource.org/licenses/MIT
+
+// @host localhost:8080
+// @BasePath /
+// @query.collection.format multi
 
 func main() {
 	router := gin.Default()
@@ -17,5 +32,7 @@ func main() {
 	mahasiswaRoute := routers.NewMahasiswaRoute(router.Group("/mhs"), db)
 	mahasiswaRoute.SetupRoutes()
 
+	// route dokumentasi
+	router.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	router.Run(":8080")
 }
