aed2ff6fc8a08d224f3f1032d761e0f35850178b
diusdi <riadidion@gmail.com>
2023-04-22 14:06:53 +0700

    menambahkan is_active pada model mahasiswa

--- models/mahasiswa.go
+++ models/mahasiswa.go
@@ -7,5 +7,6 @@ type Mahasiswa struct {
 	Nama              string    `json:"nama" example:"Dion"`
 	Usia              int       `json:"usia" example:"21"`
 	Gender            string    `json:"gender" example:"1"`
+	IsActive          string    `json:"is_active" example:"1"`
 	TanggalRegistrasi time.Time `json:"tanggal_registrasi" example:"2020-01-02T15:04:05Z"`
 }
